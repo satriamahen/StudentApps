552ba67c34042be51e8c568520d706a0a4dcac2c
fluoroscene <honghwe@gmail.com>
2019-04-15 03:04:48 +0700

    Form Loading Tips
    Nambahin Form Loading Tips-nya Daniel

---
+++ StudentApps/Design List/Action/LoadingTips/Loading1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace StudentApps.Design_List.Action.LoadingTips
+{
+    partial class Loading1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loading1));
+            this.SuspendLayout();
+            // 
+            // Loading1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.Name = "Loading1";
+            this.Text = "Loading";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ StudentApps/Design List/Action/LoadingTips/Loading1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentApps.Design_List.Action.LoadingTips
+{
+    public partial class Loading1 : Form
+    {
+        public Loading1()
+        {
+            InitializeComponent();
+        }
+    }
+}
